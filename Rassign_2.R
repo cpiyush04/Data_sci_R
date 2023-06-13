316904fa80b671e8e0031f16587cd999c5fc81dc
Piyush Chandra <126682959+cpiyush04@users.noreply.github.com>
2023-06-13 22:30:44 +0530

    Update Rassign_2.R
    updated assignments

--- Rassign_2.R
+++ Rassign_2.R
@@ -69,3 +69,34 @@ ques<-html %>% html_elements(".s-link") %>% html_text()
 ques<-ques[2:16]
 df <- data.frame("Title of question"= ques,"number of views"=views,"number of answers"=ans, "number of votes"=votes)
 
+#----------------------------------QUES-e----------------------------------------
+
+half_tab <- function() {
+  count <- 0  
+  wholetab <- 100
+  halftab <- 0
+  while (TRUE) {
+    count <- count + 1
+    item_pull <- sample(c(rep("whole", wholetab), rep("half",halftab)), 1, replace = TRUE)
+    if (item_pull == "half") {
+      return(count)
+    }
+    if (item_pull == "whole") {
+     wholetab <- wholetab - 1
+      halftab <- halftab + 2
+    }
+  }
+}
+
+simulations <- 10000
+total_days <- 0
+
+for (i in 1:simulations) {
+  total_days <- total_days + half_tab()
+}
+
+average_days <- total_days / simulations
+
+cat("on average, it will take about", average_days, "days.")
+
+
